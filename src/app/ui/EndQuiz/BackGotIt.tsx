b4d8ca97aa01a11613110591dd7810b562ab5925
Simfort <lemondzavadavid265@gmail.com>
2025-08-07 15:48:07 +0300

    fix

--- src/app/ui/EndQuiz/BackGotIt.tsx
+++ src/app/ui/EndQuiz/BackGotIt.tsx
@@ -3,7 +3,7 @@ import { ArrowLeft, ArrowRight } from "lucide-react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function BackGotIt({
   path,
@@ -12,25 +12,32 @@ export default function BackGotIt({
   path: string;
   backpath: string;
 }) {
-  const search = useSearchParams().toString();
+  const search = useSearchParams();
   const [searchParam, setSearchParam] = useState("");
-
+  useEffect(() => {
+    setSearchParam(search.toString());
+  }, [search]);
   return (
     <section className="flex  gap-2 bg-white font-bold  justify-center fixed bottom-0 w-1/1 top-[90%]">
       <button>
-        <Link
-          href={`${backpath}?${search}`}
-          className="h-[55px] rounded-[10px] max-lg:w-[200px]  w-[256px] bg-gray-200 text-black flex items-center justify-center gap-2 max-ms:w-[180px]">
-          <ArrowLeft />
-          Back
-        </Link>
+        {" "}
+        <Suspense fallback={null}>
+          <Link
+            href={`${backpath}?${searchParam}`}
+            className="h-[55px] rounded-[10px] max-lg:w-[200px]  w-[256px] bg-gray-200 text-black flex items-center justify-center gap-2 max-ms:w-[180px]">
+            <ArrowLeft />
+            Back
+          </Link>
+        </Suspense>
       </button>
       <button>
-        <Link
-          className={`h-[55px] rounded-[10px] max-lg:w-[200px]  w-[256px] bg-[#e44240] text-white flex items-center justify-center gap-2 max-ms:w-[180px]`}
-          href={`${path}?${search}`}>
-          Got it <ArrowRight />
-        </Link>
+        <Suspense fallback={null}>
+          <Link
+            className={`h-[55px] rounded-[10px] max-lg:w-[200px]  w-[256px] bg-[#e44240] text-white flex items-center justify-center gap-2 max-ms:w-[180px]`}
+            href={`${path}?${searchParam}`}>
+            Got it <ArrowRight />
+          </Link>
+        </Suspense>
       </button>
     </section>
   );
